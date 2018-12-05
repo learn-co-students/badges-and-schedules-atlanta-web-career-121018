26021a0d3fd58be90c49e00163fd949b6d5e7904
Vanessa Prema <vanessabprema@gmail.com>
2018-12-05 15:27:01 -0500

    Conference Badges Lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  badges = []
+  arr.each do |name|
+    badges.push(badge_maker(name))
+  end
+  badges
+end
+
+def assign_rooms(arr)
+  rooms = []
+  arr.each_with_index do |name,i|
+    room = "Hello, #{name}! You'll be assigned to room #{i+1}!"
+    rooms.push(room)
+  end
+  rooms
+end
+
+def printer(arr)
+  batch_badge_creator(arr).each do |x|
+    puts x
+  end
+  assign_rooms(arr).each do |x|
+    puts x
+  end
+end
