9568cbb364581690ccf615200e8f62403a5926a9
Shivang Dave <shivang.dave@flatironschool.com>
2019-05-06 19:37:15 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_msg = []
+  array.each do |element|
+    badge_msg.append(badge_maker(element))
+  end
+  badge_msg
+end
+
+def assign_rooms(array)
+  output = []
+  for room in 1..array.length do
+    output.append("Hello, #{array[room-1]}! You'll be assigned to room #{room}!")
+  end
+  output
+end
+
+def printer(array)
+  badges = batch_badge_creator(array)
+  assignments = assign_rooms(array)
+  
+  badges.each do |badge|
+    puts badge
+  end
+  
+  assignments.each do |assign|
+    puts assign
+  end
+end
